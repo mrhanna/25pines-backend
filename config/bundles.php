71f1364f5f3240cdb028241e1716c18b570c2499
Michael Hanna <hanna.michaelr@gmail.com>
2022-03-18 19:09:06 -0500

    included and configured lexik JWT bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
